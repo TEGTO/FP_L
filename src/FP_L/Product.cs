bcc42d6f6b22e9870980cba720a065f19c1c8052
Eugene <pshonovskij@gmail.com>
2024-09-25 12:06:56 +0300

    Initial commit

---
+++ src/FP_L/Product.cs
@@ -0,0 +1,8 @@
+namespace FP_L
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ test/FP_LTests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace FP_L.Controllers.Tests
+{
+    [TestFixture]
+    public class ProductControllerTests
+    {
+        private ProductController controller;
+        private Mock<ILogger<ProductController>> mockLogger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockLogger = new Mock<ILogger<ProductController>>();
+            controller = new ProductController(mockLogger.Object);
+        }
+
+        [Test]
+        public void Get_ValidProductId_ReturnsOkObjectResult()
+        {
+            // Arrange
+            var productId = 1;
+            // Act
+            var result = controller.Get(productId);
+            // Assert
+            var okResult = result.Result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            var productResponse = okResult.Value as ProductResponse;
+            Assert.IsNotNull(productResponse);
+            Assert.That(productResponse.Id, Is.EqualTo(productId));
+            Assert.That(productResponse.Name, Is.EqualTo($"{productId} Laptop"));
+        }
+        [Test]
+        public void Get_InvalidProductId_ReturnsNotFoundResult()
+        {
+            // Arrange
+            var invalidProductId = 99;
+            // Act
+            var result = controller.Get(invalidProductId);
+            // Assert
+            Assert.IsInstanceOf<NotFoundResult>(result.Result);
+        }
+    }
+}
